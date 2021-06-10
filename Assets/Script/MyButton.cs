23198c617b391129e6dba14e92e421164babf4fe
negimogumogu <84498871+negimogumogu@users.noreply.github.com>
2021-06-10 17:26:11 +0900

    ステージ選択のスクリプトを追加します．

---
+++ Assets/Script/MyButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;//LoadSceneを使うために必要!!
+
+
+public class MyButton : MonoBehaviour {
+    public void ButtonClick() {
+        switch (transform.name) {
+            //移りたいシーンを入れる
+        case "Button1":
+            SceneManager.LoadScene("NewScene1");
+            break;
+        case "Button2":
+            SceneManager.LoadScene("NewScene2");
+            break;
+        case "Button3":
+            SceneManager.LoadScene("NewScene3");
+            break;
+        default:
+            break;
+        }
+    }
+}
